fe9cefbb2c643235dfc8018aa502e80f6bd0372a
Sergey Lem <sergey.lem@gmail.com>
2020-08-15 00:20:28 +0600

    Refactoring

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import cars from './data/cars-data';
 
 const App = () => {
 
+  //window.soundManager.setup({ debugMode: false }); //disable logs at react-sound
+
   const [data, setData] = useState(cars.data);
   const [maxArray, setMaxArray] = useState(cars.maxArray);
   const [rndImages, setRndImages] = useState(cars.rndImages);
@@ -19,35 +21,33 @@ const App = () => {
   const [gameOver, setGameOver] = useState(cars.gameOver);
 
 
+  // eslint-disable-next-line
   useEffect(() => initialState(), []);
 
   const initialState = () => {
-
-    console.log('maxArray: ' + maxArray);
-
-    // const rndIndexes = getFourRndImages(cars.maxArray); //Получить 4 случайных индекса из массива cars         
-    // const questionIndex = Math.floor(Math.random() * 4); //Индекс озвученной картинки 
-    // setRndImages(rndIndexes); //Запомнить 4 картинки 
-    // setQuestionIndex(questionIndex); //Запомнить индекс озвученной картинки
+    const rndIndexes = getFourRndImages(maxArray); //Получить 4 случайных индекса из массива cars         
+    const questionIndex = Math.floor(Math.random() * 4); //Индекс озвученной картинки 
+    setRndImages(rndIndexes); //Запомнить 4 картинки 
+    setQuestionIndex(questionIndex); //Запомнить индекс озвученной картинки
     //    this.setState({ rndImages: rndIndexes, questionIndex: questionIndex }); //Запомнить 4 картинки и индекс озвученной картинки 
   }
 
   const changeCars = (buttonIndex) => {
-    let tmpArray = cars.data;  //cars записываю во временный массив 
-    const rndImages = cars.rndImages; //ХРАНЯТСЯ ИНДЕКСЫ!
-    const maxArray = cars.maxArray;
-    const idImage = tmpArray[rndImages[buttonIndex]].id; //Нахожу картинку по индексу кнопки
+    let tmpArray = data;  //cars записываю во временный массив 
+    const _rndImages = rndImages; //ХРАНЯТСЯ ИНДЕКСЫ!
+    const _maxArray = maxArray;
+    const idImage = tmpArray[_rndImages[buttonIndex]].id; //Нахожу картинку по индексу кнопки
 
     const imageIndex = tmpArray.findIndex(idImg => idImg.id === idImage); //Нахожу индекс картинки в cars по картинке
     tmpArray.splice(imageIndex, 1);  //Удаляю эту картинку из cars
 
-    const rndIndexes = getFourRndImages(maxArray - 1); //Получить 4 случайных индекса из массива cars
-    const questionIndex = Math.floor(Math.random() * 4); //Индекс озвученной картинки 
+    const rndIndexes = getFourRndImages(_maxArray - 1); //Получить 4 случайных индекса из массива cars
+    const _questionIndex = Math.floor(Math.random() * 4); //Индекс озвученной картинки 
 
     setData(tmpArray);
-    setMaxArray(maxArray - 1);
+    setMaxArray(_maxArray - 1);
     setRndImages(rndIndexes);
-    setQuestionIndex(questionIndex);
+    setQuestionIndex(_questionIndex);
     setCorrectAnswer(false);
     setPlayResultAnswer(false);
 
@@ -58,13 +58,13 @@ const App = () => {
   };
 
   const checkAnswer = (buttonIndex) => {
-    if (buttonIndex === cars.questionIndex) {
-      if (cars.data.length > 4) {
+    if (buttonIndex === questionIndex) {
+      if (data.length > 4) {
         setQuestionIndex(buttonIndex);
         setCorrectAnswer(true);
         setPlayResultAnswer(true);
         setPlayError(false);
-        setCountCorrectAnswer(cars.countCorrectAnswer + 1);
+        setCountCorrectAnswer(countCorrectAnswer + 1);
 
         // this.setState({ questionIndex: buttonIndex, correctAnswer: true, playResultAnswer: true, playError: false, countCorrectAnswer: this.state.countCorrectAnswer + 1 });
       }
@@ -76,15 +76,15 @@ const App = () => {
     else {
       setCorrectAnswer(false);
       setPlayResultAnswer(true);
-      setPlayError(!cars.playError);
-      setCountWrongAnswer(cars.countWrongAnswer + 1);
+      setPlayError(!playError);
+      setCountWrongAnswer(countWrongAnswer + 1);
 
       // this.setState((state) => ({ correctAnswer: false, playResultAnswer: true, playError: !state.playError, countWrongAnswer: this.state.countWrongAnswer + 1 }));
     }
   };
 
   const repeatAnswer = () => {
-    setPlayQuestion(!cars.playQuestion)
+    setPlayQuestion(!playQuestion)
     // this.setState((state) => ({ playQuestion: !state.playQuestion }));
   };
 
@@ -92,10 +92,10 @@ const App = () => {
   let nextBtn = false;
   let playSoundQuestion, playSoundAnswer, playSoundGameOver;
 
-  if (cars.playResultAnswer === true) {
-    if (cars.correctAnswer === false) {
+  if (playResultAnswer === true) {
+    if (correctAnswer === false) {
       nextBtn = false;
-      if (cars.playError === true) {
+      if (playError === true) {
         playSoundAnswer = (<PlaySound urlStr="/assets/sounds/e1.mp3" />);
 
       } else {
@@ -107,19 +107,19 @@ const App = () => {
     };
   }
 
-  if (cars.playQuestion === true) {
+  if (playQuestion === true) {
     playSoundQuestion = (
-      <PlaySound urlStr={cars.data[cars.rndImages[cars.questionIndex]].sound} />
+      <PlaySound urlStr={data[rndImages[questionIndex]].sound} />
     );
   }
   else {
     playSoundQuestion = (//Включил <div>, чтобы был рендеринг 
       <div>
-        <PlaySound urlStr={cars.data[cars.rndImages[cars.questionIndex]].sound} />
+        <PlaySound urlStr={data[rndImages[questionIndex]].sound} />
       </div>
     );
   }
-  if (cars.gameOver === true) {
+  if (gameOver === true) {
     playSoundGameOver = (
       <PlaySound urlStr="/assets/sounds/s1.wav" />
     );
@@ -134,12 +134,12 @@ const App = () => {
         <img
           className='image-btn'
           onClick={() => checkAnswer(0)}
-          src={cars.data[cars.rndImages[0]].img}
+          src={data[rndImages[0]].img}
           alt="Image1" />
         <img
           className='image-btn'
           onClick={() => checkAnswer(1)}
-          src={cars.data[cars.rndImages[1]].img}
+          src={data[rndImages[1]].img}
           alt="Image2" />
         <img
           className='next-repeat-btn'
@@ -149,22 +149,22 @@ const App = () => {
           alt="ImageRepeat" />
       </div>
       <div className="container3">
-        <h5>{cars.countCorrectAnswer} : {cars.countWrongAnswer}</h5>
+        <h5>{countCorrectAnswer} : {countWrongAnswer}</h5>
       </div>
       <div className="container2">
         <img
           className='image-btn'
           onClick={() => checkAnswer(2)}
-          src={cars.data[cars.rndImages[2]].img}
+          src={data[rndImages[2]].img}
           alt="Image3" />
         <img
           className='image-btn'
           onClick={() => checkAnswer(3)}
-          src={cars.data[cars.rndImages[3]].img}
+          src={data[rndImages[3]].img}
           alt="Image4" />
         <img
           className={nextBtn ? "next-repeat-btn" : "next-repeat-btn-disabled"}
-          onClick={nextBtn ? () => changeCars(cars.questionIndex) :
+          onClick={nextBtn ? () => changeCars(questionIndex) :
             null}
           // src={require('/assets/arrows/arrow2.png')}
           src="/assets/arrows/arrow2.png"
